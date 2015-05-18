4c257f010500013457fee8578fda2fb1f78fcde3
carlos <cg_47@hotmail.com>
2015-05-18 16:23:37 -0430

    ....

--- controlador/empleado/con_mostrar_empleado_registrado.php
+++ controlador/empleado/con_mostrar_empleado_registrado.php
@@ -9,34 +9,28 @@ $mostrar=$consulta-> mostrar ($pgconn);
    if(pg_num_rows($mostrar)>0){
 ?>
 
-<style type="text/css">
-<!--
-.style1 {color: #000000}
-.style3 {font-family: Arial, Helvetica, sans-serif; font-weight: bold; color: #000000; }
--->
-</style>
-
-
-<table width=" " border="2" bgcolor="#dcdcdc" align="center" id="tablaregistro" >
-<thead >
- <tr bgcolor="#000000" >
- <th align="center" bgcolor="#eeece1"><span class="style3">Nombre</span></th>
- <th align="center" bgcolor="#eeece1"><span class="style3">Apellido</span></th>
- <th align="center" bgcolor="#eeece1"><span class="style3">Cédula</span></th>
-</tr>
-</thead>
 <?php
+require("../css/css.php");
+
 for($i=0; $i<pg_num_rows($mostrar); $i++){
 $row= pg_fetch_array($mostrar,$i,PGSQL_ASSOC);
+
 $emp_nom = $row["emp_nom"];
 $emp_ape = $row["emp_ape"];
 $emp_ced = $row["emp_ced"];
 ?>
-<tr>
-<td align="center" bgcolor="#f7f6f1"><span class="style1"><?php echo $emp_nom;?></span>
-<td align="center" bgcolor="#f7f6f1"><span class="style1"><?php echo $emp_ape;?></span>
-<td align="center" bgcolor="#f7f6f1"><span class="style1"><?php echo $emp_ced;?></span>
-    <?php }?>
-</span>
-</table>
+
+<div class="container">
+
+<div class="form-group">Nombre: <?php echo $emp_nom;?></div>
+<div class="form-group">Apellido: <?php echo $emp_ape;?></div>
+<div class="form-group">Cedula: <?php echo $emp_ced;?></div>
+
+</div>
+<?php }?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+
 <?php }?>
